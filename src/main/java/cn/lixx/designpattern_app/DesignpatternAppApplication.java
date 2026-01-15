bf1674fe0b0e5b5e961a15cf1ee70e20999b10c8
Lixx <i@lixx.cn>
2026-01-15 09:28:25 +0800

    init

---
+++ src/main/java/cn/lixx/designpattern_app/DesignpatternAppApplication.java
@@ -0,0 +1,13 @@
+package cn.lixx.designpattern_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignpatternAppApplication.class, args);
+	}
+
+}
